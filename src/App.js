1dc97748ccca91035c26aa79978b05a22286cca1
kimjeongmin <kij9451@naver.com>
2023-08-17 20:50:30 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -4,21 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          // 깃 테스트
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
