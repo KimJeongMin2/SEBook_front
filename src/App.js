19476fec3dbbe0ac4a7b94582c099cf80cad5567
kimjeongmin <kij9451@naver.com>
2023-08-17 18:46:48 +0900

    커밋 테스트

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          // 깃 테스트
         >
           Learn React
         </a>
